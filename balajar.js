03c7482ea2c0a43ee4ce5927e0e58f0ba5b63cc5
Fattah-sys <muhammad.fattah@student.alhazen.academy>
2024-10-18 14:09:06 +0700

    balajar.js

--- balajar.js
+++ balajar.js
@@ -3,3 +3,27 @@ alert("selamat datang");
 alert("Ini adalah alert");
 
 prompt("siapa nama kamu ?:")
+
+var nama = "Fattah";
+console.log(nama);
+
+
+let umur = 20;
+console.log(umur);
+
+
+const alamat= "bekasi";
+console.log(alamat);
+
+angka = 4;
+angka2 = 8;
+console.log(angka*angka2);
+
+let number1 = prompt("masukkan angka 1:");
+let number2 = prompt("masukkan angka 2:");
+let result = parseInt(number1)*(number2);
+alert(result)
+
+fruit=["banana","apple","orange"];
+
+alert(fruit[2]);
